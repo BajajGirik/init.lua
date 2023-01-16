aed7972b709584aca09b267dad5ab5c79cccf32d
Girik Bajaj <bajajgirik2010@gmail.com>
2023-01-16 11:16:40 +0530

    Change directory name and remove unneeded config

--- lua/girik/init.lua
+++ lua/girik/init.lua
@@ -1,8 +1,8 @@
-require("theprimeagen.set")
-require("theprimeagen.remap")
+require("girik.set")
+require("girik.remap")
 
 local augroup = vim.api.nvim_create_augroup
-local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+local GirikGroup = augroup('Girik', {})
 
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
@@ -23,7 +23,7 @@ autocmd('TextYankPost', {
 })
 
 autocmd({"BufWritePre"}, {
-    group = ThePrimeagenGroup,
+    group = GirikGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
