9741e06e5e98ad2f9c8649942035b1b23cc33843
Girik Bajaj <bajajgirik2010@gmail.com>
2023-04-04 11:36:28 +0530

    Simplify commenting process

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -2,13 +2,8 @@ require('Comment').setup({
     toggler = {
         ---Line-comment toggle keymap
         line = '<leader>/',
-        ---Block-comment keymap
-        block = '<leader>c/',
     },
     opleader = {
-        ---Line-comment toggle keymap
-        line = '<leader>c/',
-        ---Block-comment keymap
         block = '<leader>/',
     }
 })
